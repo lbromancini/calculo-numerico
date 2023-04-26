b03f6914961c0ca76a300c487e2746a2a8178b77
enzo <109364060+lbromancini@users.noreply.github.com>
2023-04-25 22:15:44 -0300

    Update cordas.m

--- cordas.m
+++ cordas.m
@@ -1,21 +1,16 @@
-% ex: a) f(x) = exp(x)-sin(x)-2 / [1;1,2] / e = 10^-5
-% R: 1,05413
+% MÃ©todo das cordas
 
-% 3*x^3 - x + 1/2 - sin(x^(1/3))
-
-% log(x^2) + x^3 - 4*x + 3/2 
-
-% Declaração da função
+% DeclaraÃ§Ã£o da funÃ§Ã£o
 syms x
-f = input('Insira a equação: ')
-f = inline(f,'x') % Torna possível utilizar a função no código
+f = input('Insira a equaÃ§Ã£o: ')
+f = inline(f,'x') % Torna possÃ­vel utilizar a funÃ§Ã£o no cÃ³digo
 
 % Input dos valores iniciais
 a = input('Digite o valor de a: ')
 b = input('Digite o valor de b: ')
 e = input('Digite o valor da tolerancia: ')
 
-% Aplicação do método
+% AplicaÃ§Ã£o do mÃ©todo
 fa = f(a)
 fb = f(b)
 
@@ -23,22 +18,22 @@ x = ( (a*fb) - (b*fa) ) / (fb - fa)
 
 fx = f(x)
 
-if fa*fx < 0 % Se fa*fx for negativo, meu intervalo é [a,x]
+if fa*fx < 0 % Se fa*fx for negativo, meu intervalo Ã© [a,x]
      a = a
      b = x
-else % Caso contrário, meu intervalo é [x,b]
+else % Caso contrÃ¡rio, meu intervalo Ã© [x,b]
      a = x
      b = b
 end
 
-% Repete o método até o critério de parada ser cumprido
+% Repete o mÃ©todo atÃ© o critÃ©rio de parada ser cumprido
 while (abs(fa) >= e && abs(fb) >= e && (b-a) >= e)
     
     fa = f(a)
     fb = f(b)
     
     if fa*fb > 0 % fa e fb precisam ter sinais opostos para convergir
-        fprintf('Intervalo não converge, digite o intervalo novo')
+        fprintf('Intervalo nÃ£o converge, digite o intervalo novo')
         break
     end
 
@@ -56,4 +51,4 @@ while (abs(fa) >= e && abs(fb) >= e && (b-a) >= e)
     
 end
     
-fprintf('O intervalo final é [%.5f;%.5f] \n', a, b)
+fprintf('O intervalo final Ã© [%.5f;%.5f] \n', a, b)
